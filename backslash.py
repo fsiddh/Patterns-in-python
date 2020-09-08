b2a7cc71eafd7749dd180e3a7b0d15ede9c40858
fsiddh <jasonshaw546@gmail.com>
2020-09-08 15:44:12 +0530

    new method of backslash

--- backslash.py
+++ backslash.py
@@ -1,3 +1,4 @@
+#Method 1
 spaces = 0
 for i in range(0, 5):
     for j in range(0, 5):
@@ -7,3 +8,12 @@ for i in range(0, 5):
             print(' ', end=' ')
     print()
     spaces += 1
+
+# Method 2
+for i in range(0, 5):
+    for j in range(0, 5):
+        if j == i:
+            print('*', end=' ')
+        else:
+            print(' ', end=' ')
+    print()
