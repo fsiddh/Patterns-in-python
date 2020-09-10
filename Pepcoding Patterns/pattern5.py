dc69e2b9747ef9af16905766d4d7a271c0362e54
fsiddh <jasonshaw546@gmail.com>
2020-09-10 22:59:53 +0530

    pattern5 to do

--- Pepcoding Patterns/pattern5.py
+++ Pepcoding Patterns/pattern5.py
@@ -1,17 +1,16 @@
-x = int(input())
-left = x // 2
-right = x // 2
+n = int(input())
+spaces = n // 2
+stars = 1
 
-for i in range(0, x):
-    for j in range(0, x):
-        if left == j or right == j:
-            print('*', end='\t')
-        else:
-            print('', end='\t')
-    print()
-    if i < x//2:
-        left -= 1
-        right += 1
+for i in range(1, n+1):
+    for j in range(1, spaces+1):
+        print('\t')
+    for j in range(1, stars+1):
+        print('*\t')
+    if i <= n // 2:
+        spaces -= 1
+        stars += 2
     else:
-        left += 1
-        right -= 1
+        spaces += 1
+        stars -= 2
+    print()
