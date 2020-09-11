9f6a237f81e86d005545f7de64758f6302fc769a
fsiddh <jasonshaw546@gmail.com>
2020-09-11 12:21:38 +0530

    pattern 5 completed

--- Pepcoding Patterns/pattern5.py
+++ Pepcoding Patterns/pattern5.py
@@ -2,15 +2,16 @@ n = int(input())
 spaces = n // 2
 stars = 1
 
-for i in range(1, n+1):
-    for j in range(1, spaces+1):
-        print('\t')
-    for j in range(1, stars+1):
-        print('*\t')
-    if i <= n // 2:
+for i in range(0, n):
+    for sp in range(0, spaces):
+        print('\t', end='')
+    for st in range(0, stars):
+        print('*\t', end='')
+    print()
+
+    if i < n//2:
         spaces -= 1
         stars += 2
     else:
         spaces += 1
         stars -= 2
-    print()
